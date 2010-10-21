57f16ddc44384620fa3094865992b4f282a7e698
Alex Popiel <apopiel@marchex.com>
2010-10-21 16:30:11 -0700

    Restructured post handling, starting on the templating, registration.

--- node/quiz.js
+++ node/quiz.js
@@ -2,14 +2,20 @@ var ws = require("websocket-server"),
     http = require('http'),
     form = require('../lib/www-forms.js'),
     url = require('url'),
+    querystring = require('querystring'),
+    redis = require('redis'),
+    mu = require('mu'),
     sys = require('sys');
+
+mu.templateRoot = '../templates';
 
 var wsport = 8008;
 var webport = 8080;
 
 var wsserver = ws.createServer();
+var rclient = redis.createClient();
 
-var routes = {
+var getRoutes = {
     '/' : function(request, response) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('placeholder');
@@ -26,7 +32,78 @@ var routes = {
         response.write("Disallow: /\n");
         response.end();
     },
+    '/register' : function(request, response) {
+//        response.write(require.paths.join("<BR>"));
+//        response.end();
+        mu.render('register', {}, {}, function(err, output) {
+            if (err) { throw err; }
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            output.addListener('data', function(c) { response.write(c); });
+            output.addListener('end',  function()  { response.end();    });
+        });
+    },
     '/vxml' : function(request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('vxml placeholder');
+    },
+};
+
+function redirect(response, location, args) {
+    var encoded = new Array();
+    for (var key in args) {
+        encoded.push(querystring.escape(key) + "=" + querystring.escape(args[key]));
+    }
+    if (encoded.length > 0) {
+        location = location + '?' + encoded.join('&');
+    }
+    response.writeHead(302, {'Location': location, 'Content-Type': 'text/html'});
+    response.write('<html><head><title>Moved</title></head><body><a href="' + location +'">Oops.</a></body></html>');
+    response.end();
+}
+
+var postRoutes = {
+    '/' : function(request, response, args) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('placeholder');
+    },
+    '/register' : function(request, response, args) {
+        var errors = new Array();
+        if (!args.caller_name  .match(/^.+$/))                             { errors.push({message: "You must specify your name."                                  }); }
+        if (!args.caller_number.match(/^\(?\d\d\d\)?-?\d\d\d-?\d\d\d\d$/)) { errors.push({message: "You must specify a 10-digit phone number including area code."}); }
+        if (!args.caller_email .match(/^[^@, ]+@[^@, ]+$/))                { errors.push({message: "You must specify a valid email address."                      }); }
+        if (errors.length > 0) {
+            mu.render('register', {errors: errors}, {}, function(err, output) {
+                if (err) { throw err; }
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                output.addListener('data', function(c) { response.write(c); });
+                output.addListener('end',  function()  { response.end();    });
+            });
+            return;
+        }
+        rclient.getset("quiz:" + args.caller_number + ":active", "true", function(err, reply) {
+            if (reply == "true") {
+                
+            }
+        });
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('placeholder');
+    },
+    '/ping' : function(request, response, args) {
+        response.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        response.write("Call from " + args.caller_name + " <"+ args.caller_number + "> (calling: " + args.called_number + ") complete.  Status: " + args.call_status);
+        response.end();
+    },
+    '/vxml/question' : function(request, response, args) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write("User-agent: discobot\n");
+        response.write("Disallow: /\n");
+        response.write("User-agent: *\n");
+        response.write("Disallow: /\n");
+        response.end();
+    },
+    '/vxml' : function(request, response, args) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('vxml placeholder');
     },
@@ -49,30 +126,39 @@ sys.log("ws: listening on " + wsport);
 // listen for posts, and then broadcast messages to ws clients
 var webserver = http.createServer(function (request, response) {
     if (request.method == 'POST') {
-        post_handler(request, function(request_data) {
-            response.writeHead(200, {
-                "Content-Type": "text/plain"
-            });
-            response.end("ok");
-            sys.log("POST-a-licious");
-            // incr for vote, then broadcast new counts
-            if (request_data.call_status) {
-                wsserver.broadcast("Call from " + request_data.caller_name + " <"+ request_data.caller_number + "> (calling: " + request_data.called_number + ") complete.  Status: " + request_data.call_status);
-            }
-            else {
-                wsserver.broadcast("Incoming call from: " + request_data.caller_number + " (calling: " + request_data.called_number + ") acc/cmp: " + request_data.acc + "/" + request_data.cmp);
-            }
-        });
+        var pathname = url.parse(request.url).pathname;
+        if (postRoutes[pathname] === undefined) {
+            not_found(request, response);
+        }
+        else {
+            post_handler(request, function(request_data) { postRoutes[pathname].call(this, request, response, request_data) });
+        }
+//        
+//        post_handler(request, function(request_data) {
+//            response.writeHead(200, {
+//                "Content-Type": "text/plain"
+//            });
+//            response.write("Call from " + request_data.caller_name + " <"+ request_data.caller_number + "> (calling: " + request_data.called_number + ") complete.  Status: " + request_data.call_status);
+//            response.end();
+//            sys.log("POST-a-licious");
+//            // incr for vote, then broadcast new counts
+//            if (request_data.call_status) {
+//                wsserver.broadcast("Call from " + request_data.caller_name + " <"+ request_data.caller_number + "> (calling: " + request_data.called_number + ") complete.  Status: " + request_data.call_status);
+//            }
+//            else {
+//                wsserver.broadcast("Incoming call from: " + request_data.caller_number + " (calling: " + request_data.called_number + ") acc/cmp: " + request_data.acc + "/" + request_data.cmp);
+//            }
+//        });
     }
     else if (request.method == 'GET') {
         // send vxml
         // based off query params?
         var pathname = url.parse(request.url).pathname;
-        if (routes[pathname] === undefined) {
+        if (getRoutes[pathname] === undefined) {
             not_found(request, response);
         }
         else {
-            routes[pathname].call(this, request, response);
+            getRoutes[pathname].call(this, request, response);
         }
     }
     else {
@@ -92,6 +178,9 @@ function post_handler(request, callback) {
     if (request.method == 'POST') {
         request.addListener('data', function(c) {
             _content += c;
+        });
+        request.addListener('end', function() {
+            callback(querystring.parse(_content));
         });
     }
 }
---
+++ templates/register.mu
@@ -0,0 +1,17 @@
+<html>
+<head><title>Signup form</title></head>
+<body>
+  {{#errors}}
+  <div class="error">
+    {{message}}
+  </div>
+  {{/errors}}
+  <form method="post" action="/register">
+    <table>
+      <tr><th><label for="caller_name"  >Caller name:  </label></th><td><input type="text" id="caller_name"   name="caller_name"  ></td></tr>
+      <tr><th><label for="caller_number">Caller number:</label></th><td><input type="text" id="caller_number" name="caller_number"></td></tr>
+      <tr><th><label for="caller_email" >Caller email: </label></th><td><input type="text" id="caller_email"  name="caller_email" ></td></tr>
+      <tr><td>&nbsp</td><td><input type="submit" id="Submit" name="Submit"></td></tr>
+    </table>
+  </form>
+</body>
